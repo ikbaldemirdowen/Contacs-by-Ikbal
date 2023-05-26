27bb518ad34494c709d2bac410adb2fa863023c7
Ikbal Demirdoven <ikbaldemirdoven@icloud.com>
2023-05-25 21:47:35 -0700

    Initial Commit

---
+++ Contacs by Ikbal/Contacs_by_IkbalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Contacs_by_IkbalApp.swift
+//  Contacs by Ikbal
+//
+//  Created by Ikbal Demirdoven on 2023-05-25.
+//
+
+import SwiftUI
+
+@main
+struct Contacs_by_IkbalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
